1092f29afdd3cf919822c572957663f0e27bfc16
Julien <chouteau49@gmail.com>
2023-05-18 14:28:32 +0200

    Update

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,5 +1,7 @@
-DROP TABLE IF EXISTS user;
+-- Initialize the database.
+-- Drop any existing data and create empty tables.
 
+DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS post;
 
 CREATE TABLE user (
